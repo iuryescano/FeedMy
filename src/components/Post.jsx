d6d6a3ce8d8399995d533c4ad1e95da4a56a8e8b
iuryescano <iurycordeiro15@gmail.com>
2025-03-13 17:29:05 -0300

    :sparkles: feat: Adding disable in button for comment and adding cleancode concept

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,7 +35,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange(){
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(){
+    event.target.setCustomValidity(`Esse campo é obrigatório!`)
   }
 
   function deleteComment(commentToDelete){
@@ -44,6 +49,8 @@ export function Post({ author, publishedAt, content }) {
     });
     setComments(commentsWithoutDeleteOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0; //variable para ser cleanCode
 
   return (
     <article className={styles.post}>
@@ -80,10 +87,17 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button 
+            type="submit" 
+            disabled={isNewCommentEmpty}
+          >
+              Publicar
+          </button>
         </footer>
       </form>
 
